ded0d13966e86d38aee2b1e6ca1c4019272522d1
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2024-02-07 10:55:00 +0000

    Binding Static Resources

---
+++ 10/WpfBindingStaticResources/Site.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBindingStaticResources
+{
+    public class Site
+    {
+        public int ID { get; set; }
+        public String Nome { get; set; }
+        public String Endereco { get; set; }
+    }
+}
